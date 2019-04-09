ff1afa654d7534d21c4899618f3294f141a0cfec
denisia.blaga <denisia.blaga@accesa.eu>
2019-04-09 18:33:13 +0300

    Project setup

---
+++ masd/src/main/java/com/masd/masd/PocketManagerApplication.java
@@ -0,0 +1,13 @@
+package com.masd.masd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketManagerApplication.class, args);
+	}
+
+}
